8de9f73a3b8e7389262d1807f37afbf93329b7df
Matheus-Dias-G <neotromd@gmail.com>
2020-06-17 18:56:56 -0300

    sistema organizacao

---
+++ conta.py
@@ -0,0 +1,14 @@
+class Conta:
+    def __init__(self, nome, cpf, idade):
+        self.nome = nome
+        self.cpf = cpf
+        self.idade = idade
+
+class Cliente:
+    def __init__(self, sacar, despositar, saldo):
+        self.sacar = sacar
+        self.depositar = despositar
+        self.saldo = saldo
+
+
+
